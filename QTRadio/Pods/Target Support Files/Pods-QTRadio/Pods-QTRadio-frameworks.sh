d2ad59524d12b04f7a47df8885a989851317452e
enricashi <aishiqichun@163.com>
2018-01-28 13:31:51 +0800

    更新第三方框架，并且集成Reveal-SDK

--- QTRadio/Pods/Target Support Files/Pods-QTRadio/Pods-QTRadio-frameworks.sh
+++ QTRadio/Pods/Target Support Files/Pods-QTRadio/Pods-QTRadio-frameworks.sh
@@ -104,6 +104,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Alamofire/Alamofire.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Kingfisher/Kingfisher.framework"
+  install_framework "${PODS_ROOT}/Reveal-SDK/RevealServer-13/iOS/RevealServer.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
